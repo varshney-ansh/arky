efdcd9bb720b9faa7f5a9ce0e88be84202c659c3
Anjali <anjali91408115@gmail.com>
2025-09-19 03:38:31 +0530

    test1

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,29 @@
+"use client";
 import MainChat from "@/components/MainChat/MainChat";
 import NavBar from "@/components/MainChat/NavBar";
 import SideBar from "@/components/MainChat/SideBar";
-
-
+import { useState } from "react";
 import Image from "next/image";
 import BrandLogo from "@/components/brandLogo/brandLogo";
 
 export default function Home() {
+ const [isOpen, setIsOpen] = useState(true);
+
   return (
 
     <section>
      
 
 
-<div className="flex">
+<div className="flex  ">
 
 
-<div className="h-[100vh]">
-<SideBar/>
+<div className="h-[100vh] ">
+<SideBar isOpen={isOpen} />
 
 </div>
-<div className="h-[100vh] w-[100%]  ">
- <NavBar />
+<div className="h-screen w-[100%] flex flex-col justify-between transition-all duration-500  ">
+ <NavBar isOpen={isOpen} setIsOpen={setIsOpen} />
 <MainChat />
  
  </div>
