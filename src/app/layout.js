9e93513800d26cd7f6fae9ca6e09dfa6d3b35d44
Aditya Singh <adityarajawat6397@gmail.com>
2025-09-20 01:38:34 +0530

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -112,7 +112,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,200" />
       </head>
       <body
         className={`${geistSans.variable} ${esRebound.variable} antialiased`}
