b2439ad191a60a431531ad30edb99cfb2973600e
Anshvar1 <anshvar22@gmail.com>
2025-09-20 01:24:31 +0530

    + whiteboard complete

--- src/app/board/[boardId]/page.jsx
+++ src/app/board/[boardId]/page.jsx
@@ -1,12 +1,15 @@
 import WhiteBoard from '@/components/whiteboard/whiteBoard';
 import styles from './arc.module.css';
+import { ReactFlowProvider } from '@xyflow/react';
 
 const BoardPage = async ({ params }) => { 
     const { boardId } = await params;
     console.log("Board ID:", boardId);
     return (
         <div className='w-full h-screen'>
-            <WhiteBoard />
+            <ReactFlowProvider>
+                <WhiteBoard />
+            </ReactFlowProvider>
         </div>
     );
 }
